8913e8c387412b7710ec56407d159ff872213115
Den <den.3.14ven@gmail.com>
2018-08-30 14:33:12 +0300

    start

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <malloc.h>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    //here
+    //new
+    //op
+    int a;
+    return 0;
+}
