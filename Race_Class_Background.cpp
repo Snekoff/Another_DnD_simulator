75309061c98619c3e1fafad4573d6b6ca9cb4b36
Den <den.3.14ven@gmail.com>
2018-08-31 21:20:47 +0300

    version 0.0.617

--- Race_Class_Background.cpp
+++ Race_Class_Background.cpp
@@ -6,16 +6,17 @@
 class race{
 private:
     int type;
-    int height,weight,age;
-    int minHeight[61];
-    int maxHeight[61];
-    int minWeight[61];
-    int maxWeight[61];
-    int minAge[61];
-    int maxAge[61];
 protected:
+    int height,weight,age;
+    int minHeight[39]= {6,4,4,5,5,5,5,5,5,5,5,3,3,3,3,5,5,5,5,5,5,3,3,3,5,5,5,6,5,5,5,5,5,5,5,5,5,5,5};
+    int maxHeight[39]= {6,5,5,6,6,6,6,6,6,6,6,4,4,4,4,6,6,6,6,6,6,3,3,3,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6};
+    int minWeight[39]={10};
+    int maxWeight[39]={140};
+    int minAge[39] = {3};
+    int maxAge[39] = {120};
+    int Str,Dex,Con,Int,Wis,Cha;
     Spell spell;
-    std::string raceFeatures;// Movement,Size,Height,Weight,Darkvision,skill like firebreath,type of damage resistance,Languages//
+    std::string raceFeatures;// Abilities0-5,Movement,Size,Darkvision,skill like firebreath,type of damage resistance,Languages//
 public:
     race(){
         type = 0;
@@ -58,159 +59,159 @@ public:
     void subClassFeatures(){}
 };
 /*
-Dragonborn
+Dragonborn 0
 Str +2; Cha +1
 6+
 
-Dwarf (Duergar)
+Dwarf (Duergar) 1
 Str +1; Con +2
 4-5
 
-Dwarf (Hill)
+Dwarf (Hill) 2
 Con +2; Wis +1
 4-5
 
-Dwarf (Mountain)
+Dwarf (Mountain) 3
 Str +2; Con +2
 4-5
 
-Elf (Drow)
+Elf (Drow)4
 Dex +2; Cha +1
 5-6
 
-Elf (Eladrin)
+Elf (Eladrin)5
 Dex +2; Int +1
 5-6
 
-Elf (Eladrin)
+Elf (Eladrin)6
 Dex +2; Cha +1
 5-6
 
-Elf (High)
+Elf (High)7
 Dex +2; Int +1
 5-6
 
-Elf (Sea)
+Elf (Sea)8
 Dex +2; Con +1
 5-6
 
-Elf (Shadar-kai)
+Elf (Shadar-kai)9
 Dex +2; Con +1
 5-6
 
-Elf (Wood)
+Elf (Wood)10
 Dex +2; Wis +1
 5-6
 
-Gnome (Deep/Svirfneblin)
+Gnome (Deep/Svirfneblin)11
 Dex +1; Int +2
 3-3.5 Small
 
-Gnome (Forest)
+Gnome (Forest)12
 Dex +1; Int +2
 3-4 Small
 
-Gnome (Rock)
+Gnome (Rock)13
 Con +1; Int +2
 3-4 Small
 
-Goblin
+Goblin 14
 Dex +2; Con +1
 3-4 Small
 
-Half-Elf
+Half-Elf 15
 Cha +2; Any other two +1
 5-6
 
-Half-Elf (Aquatic Elf Descent)
+Half-Elf (Aquatic Elf Descent)16
 Cha +2; Any other two +1
 5-6
 
-Half-Elf (Drow Descent)
+Half-Elf (Drow Descent)17
 Cha +2; Any other two +1
 5-6
 
-Half-Elf (Moon Elf or Sun Elf Descent)
+Half-Elf (Moon Elf or Sun Elf Descent)18
 Cha +2; Any other two +1
 5-6
 
-Half-Elf (Wood Elf Descent)
+Half-Elf (Wood Elf Descent)19
 Cha +2; Any other two +1
 5-6
 
-Half-Orc
+Half-Orc 20
 Str +2; Con +1
 5-6
 
-Halfling (Ghostwise)
+Halfling (Ghostwise) 21
 Dex +2; Wis +1
 3 Small
 
-Halfling (Lightfoot)
+Halfling (Lightfoot) 22
 Dex +2; Cha +1
 3 Small
 
-Halfling (Stout)
+Halfling (Stout) 23
 Dex +2; Con +1
 3 Small
 
-Human
+Human 24
 Str +1; Dex +1; Con +1; Int +1; Wis +1; Cha +1
 5-6
 
-Human (Variant)
+Human (Variant) 25
 Any two +1
 5-6
 
-Lizardfolk
+Lizardfolk 26
 Con +2; Wis +1
 5-6
 
-Orc
+Orc 27
 Str +2; Con +1; Int -2
 6
 
-Tiefling
+Tiefling 28
 Int +1; Cha +2
 5-6
 
-Tiefling (Asmodeus)
+Tiefling (Asmodeus) 29
 Int +1; Cha +2
 5-6
 
-Tiefling (Baalzebul)
+Tiefling (Baalzebul) 30
 Int +1; Cha +2
 5-6
 
-Tiefling (Dispater)
+Tiefling (Dispater) 31
 Dex +1; Cha +2
 5-6
 
-Tiefling (Fierna)
+Tiefling (Fierna) 32
 Wis +1; Cha +2
 5-6
 
-Tiefling (Glasya)
+Tiefling (Glasya) 33
 Dex +1; Cha +2
 5-6
 
-Tiefling (Levistus)
+Tiefling (Levistus) 34
 Con +1; Cha +2
 5-6
 
-Tiefling (Mammon)
+Tiefling (Mammon) 35
 Int +1; Cha +2
 5-6
 
-Tiefling (Mephistopheles)
+Tiefling (Mephistopheles) 36
 Int +1; Cha +2
 5-6
 
-Tiefling (Variant)
+Tiefling (Variant) 37
 Int +1; Dex or Cha +2
 5-6
 
-Tiefling (Zariel)
+Tiefling (Zariel) 38
 Str +1; Cha +2
 5-6
 
