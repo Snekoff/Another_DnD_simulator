25ac0878e47a04fdad722ac250401488896e3553
Den <den.3.14ven@gmail.com>
2018-09-01 16:58:28 +0300

    version 0.0.619

--- Inventory.cpp
+++ Inventory.cpp
@@ -5,6 +5,7 @@ class Item { //all items have got damage
   std::string name;
   std::string type;
   int amount;
+  int weight;
   int damage;
   bool isweapon;
  public:
@@ -12,12 +13,26 @@ class Item { //all items have got damage
     name = "clothes";
     type = "equipment";
     amount = 1;
+    weight = 1;
     damage = 0;
     isweapon = false;
   }
   ~Item() {}
 
   void Set(){
+
+  }
+};
+
+class Weapon : public Item {
+ private:
+  int skill_required;
+  int weapon_type; // slashing/piercing etc.
+ public:
+  Weapon() {}
+  ~Weapon() {}
+
+  void SetWeapon(){
 
   }
 };
--- Race_Class_Background.cpp
+++ Race_Class_Background.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Skills_and_Spells.cpp"
 #include "UsefulFunctions.cpp"
+Existing_Types E;
 
 class Race {
  private:
@@ -27,11 +28,62 @@ class Race {
     type = a;
   }
 
-  virtual void set(int a, int b, int c, int d) {}
+  virtual void set(int a, int b, int c, int d) {
+    type = a;
+    height = b;
+    weight = c;
+    age = d;
+    SetRaceAbilityBonus();
+    RaceAbilityBonus();
+  }
 
   virtual int get() {}
 
-  void raceFeature() {}
+  void RaceFeature() {}
+
+  void SetRaceAbilityBonus() {
+    int a, b;
+    if (type < 15 || (type > 19 && type != 25 && type != 37)) {// 15-19(3)  25(2) 37(Dex or Cha)
+      printf("%s \n", "Your type got non flexible ability bonuses");
+    } else if (type == 25) {
+      printf("%s \n", "type two most wanted abilities to improve (1 - 6)");
+      std::cin >> a >> b;
+      a = Correctness(a, 1, 6);
+      b = Correctness(b, 1, 6);
+      E.race_ability_bonus[a - 1][type] += 1;
+      E.race_ability_bonus[b - 1][type] += 1;
+    } else if (type == 37) {
+      printf("%s \n", "Choose only one ability Dexterity(2) or Charisma(6) )");
+      std::cin >> a;
+      while (a != 2 && a != 6) {
+        printf("%s \n", "Choose only one ability Dexterity(2) or Charisma(6) )");
+        std::cin >> a;
+        E.race_ability_bonus[a - 1][type] += 1;
+      }
+    } else {
+      printf("%s \n", "type two most wanted abilities to improve (1 - 6)");
+      std::cin >> a >> b;
+      a = Correctness(a, 1, 6);
+      b = Correctness(b, 1, 6);
+      while (a - 1 == 5 || b - 1 == 5) {
+        printf("%s \n", "Your charisma is already raised, choose another one");
+        std::cin >> a >> b;
+        a = Correctness(a, 1, 6);
+        b = Correctness(b, 1, 6);
+      }
+      E.race_ability_bonus[a - 1][type] += 1;
+      E.race_ability_bonus[b - 1][type] += 1;
+    }
+  }
+
+  void RaceAbilityBonus(){
+    Str = E.race_ability_bonus[0][type];
+    Dex = E.race_ability_bonus[1][type];
+    Con = E.race_ability_bonus[2][type];
+    Int = E.race_ability_bonus[3][type];
+    Wis = E.race_ability_bonus[4][type];
+    Cha = E.race_ability_bonus[5][type];
+  }
 };
 
 class SubRace : public Race {
