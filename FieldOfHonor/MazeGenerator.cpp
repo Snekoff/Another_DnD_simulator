25d44430c676c360269fcbc31f10399e5b3378b0
Den <den.3.14ven@gmail.com>
2019-02-03 09:40:58 +0200

    version 0.0.824 New:   MazeGenerator     methods:     Direction     IsRightDirectoin     RightDirection     PaceLength

--- FieldOfHonor/MazeGenerator.cpp
+++ FieldOfHonor/MazeGenerator.cpp
@@ -1,26 +1,132 @@
 #include "MazeGenerator.h"
+using namespace std;
 
-MazeGenerator::MazeGenerator(Random_Generator_ *Rand_gen, vector<vector<int>> square_){
+MazeGenerator::MazeGenerator(Random_Generator_ *Rand_gen, vector<vector<int>> square_) {
 // smth here
+  deadend.resize(square_.size());
+  for (int i = 0; i < deadend.size(); i++) {
+    deadend[i].resize(square_[i].size());
+  }
 }
 
-vector<vector<int>> MazeGenerator::RoomsPlacement(vector<pair<int, int>> roomsRegions, vector<pair<int, int>> roomsEntrances, vector<vector<int>> square_){
-  for(int i = 0; i < roomsRegions.size(); i += 2){
-    pair<int, int> begining, ending;
-    begining = roomsRegions[i];
-    ending = roomsRegions[i+1];
-    int begSt = min(begining.first, begining.second), begEn = max(begining.first, begining.second);
+MazeGenerator::~MazeGenerator() {
+  for (int i = 0; i < deadend.size(); i++) {
+    deadend[i].clear();
+  }
+  deadend.clear();
+  for (int i = 0; i < entrances.size(); i++) {
+    entrances[i].clear();
+  }
+  entrances.clear();
+}
+
+bool MazeGenerator::IsNegative(int x, int y, int index) {
+  if (x < 0 || y < 0) {
+    cout << "Negative" << index << " coordinates\n";
+    return true;
+  }
+  return false;
+}
+
+// id descriptions:
+// 0 - wall
+// 1 - corridor/empty
+// 2 - room wall
+// 3 - Exit/Entrance
+// 4 - Nonbreakable wall
+// 5 - Reached Entrance
+// 6
+// 7
+vector<vector<int>> MazeGenerator::RoomsPlacement(vector<pair<int, int>> roomsRegions,
+                                                  vector<pair<int, int>> roomsEntrances,
+                                                  vector<vector<int>> square_) {
+  for (int i = 0; i < roomsRegions.size(); i += 2) {
+    pair<int, int> begin, ending;
+    begin = roomsRegions[i];
+    ending = roomsRegions[i + 1];
+    if (IsNegative(begin.first, begin.second, i / 2 + 1) || IsNegative(ending.first, ending.second, i / 2 + 1)) {
+      continue;
+    }
+    int begSt = min(begin.first, begin.second), begEn = max(begin.first, begin.second);
     int endSt = min(ending.first, ending.second), endEn = max(ending.first, ending.second);
-    for(int j = begSt; j <= begEn; j++){
-      for(int p = endSt; j <= endEn; j++){
+    for (int j = begSt; j <= begEn; j++) {
+      for (int p = endSt; j <= endEn; j++) {
         square_[j][p] = 1;
       }
     }
   }
-  for(int i = 0; i < roomsEntrances.size(); i++){
-    if(square_[roomsEntrances[i].first][roomsEntrances[i].second] != 1){
+  for (int i = 0; i < roomsEntrances.size(); i++) {
+    if (!IsNegative(roomsEntrances[i].first, roomsEntrances[i].second, i + 1)
+        && square_[roomsEntrances[i].first][roomsEntrances[i].second] == 2) {
       square_[roomsEntrances[i].first][roomsEntrances[i].second] = 3;
-    }
-    else std::cout << "Entrance #" << i+1 << " is placed not outside of the room\n";
+    } else std::cout << "Entrance #" << i + 1 << " is placed not on room wall\n";
   }
+}
+
+bool MazeGenerator::CouldMakeCorridor(int x, int y, vector<vector<int>> square_) {
+
+}
+
+int MazeGenerator::Direction(Random_Generator_ *Rand_gen) {
+
+}
+
+bool MazeGenerator::IsRightDirection(int x, int y, int direction_, vector<vector<int>> square_) {
+  if (IsNegative(x, y, -1)) return false;
+  if (y > square_[x].size() - 1 || x > square_.size() - 1) return false;
+  if (x < 2 && direction_ == 0) return false;
+  if (y < 2 && direction_ == 3) return false;
+  if (x > square_.size() - 3 && direction_ == 2) return false;
+  if (y > square_[x].size() - 3 && direction_ == 1) return false;
+  return true;
+}
+
+int MazeGenerator::RightDirection(Random_Generator_ *Rand_gen, int x, int y, int direction,
+                                  vector<vector<int>> square_) {
+  bool canMove[4] = {true, true, true, true};
+  bool deadend = false;
+  int dir = -1;// means deadend reached
+  while (!IsRightDirection(x, y, direction, square_)) {
+    canMove[direction] = false;
+    for (int j = 0; j < 4; j++) {
+      if (canMove[j]) break;
+      else if (j == 3) deadend = true;
+    }
+    if (!deadend) direction = Direction(Rand_gen);
+    else return dir;
+  }
+  dir = direction;
+  return dir;
+}
+
+int MazeGenerator::PaceLength(int difficulty_, Random_Generator_ *Rand_gen) {
+
+}
+
+pair<int, int> MazeGenerator::Move(Random_Generator_ *Rand_gen, int x, int y, vector<vector<int>> square_) {
+  pair<int, int> pos = make_pair(x, y);
+  if (IsNegative(x, y, -1)) return pos;
+  int direction = Direction(Rand_gen);
+  int minPaceLength = 2;
+  char dirDescription[4] = {'u', 'r', 'd', 'l'};
+  bool deadend = false;
+  int dirMod[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+  direction = RightDirection(Rand_gen, x, y, direction, square_);
+  if (direction == -1) return pos;
+  int x_mod = x + minPaceLength * dirMod[direction][0];
+  int y_mod = y + minPaceLength * dirMod[direction][1];
+  if (square_[x_mod][y_mod] == 0) {
+    int paceLength = PaceLength(difficulty, Rand_gen);
+    x_mod = x + paceLength * dirMod[direction][0];
+    y_mod = y + paceLength * dirMod[direction][1];
+    while (!(square_[x_mod][y_mod] == 0 && CouldMakeCorridor(x_mod, y_mod, square_))) {
+      cout << "New paceLength = " << paceLength - 1 << "\n";
+      paceLength--;
+      x_mod = x + paceLength * dirMod[direction][0];
+      y_mod = y + paceLength * dirMod[direction][1];
+    }
+    pos = make_pair(x_mod, y_mod);
+    return pos;
+  }
+  return pos;
 }
--- FieldOfHonor/MazeGenerator.h
+++ FieldOfHonor/MazeGenerator.h
@@ -5,14 +5,23 @@
 #include "../Character/character.h"
 
 
-
+// id descriptions:
+// 0 - wall
+// 1 - corridor/empty
+// 2 - room wall
+// 3 - Exit/Entrance
+// 4 - Nonbreakable wall
+// 5
+// 6
+// 7
 class MazeGenerator {
  private:
   int difficulty;
   int roomProbability;
   // entrances and exits are just places which algorithm will cross only once
-  // odd pair is start of a region and next one is the end of the region
+  // odd pair is begin of a region and next one is the end of the region
   vector<vector<pair<int, int>>> entrances;
+  vector<vector<int>> deadend;
  public:
   MazeGenerator();
 
@@ -23,18 +32,31 @@ class MazeGenerator {
  //"Mole" starts digging from entrance 1 and moving all around until there wont be any blank(massive wall regions) space
  // with few rules: The most important is that it couldn't cross exidting corridors, and "impassable" walls
  // if mole crosses the Entrance #2 then all region(Entrance #2) is blocked(marked as corridor) and cant be passed again
+ // minimum mole pace is 2 (thick* wall) *5-ft
  MazeGenerator(Random_Generator_ *Rand_gen, vector<vector<int>> square_);
  // room region is two points (start end edge)
  vector<vector<int>> RoomsPlacement(vector<pair<int, int>> roomsRegions, vector<pair<int, int>> roomsEntrances, vector<vector<int>> square_);
  // if you have some rooms, but it doesn`t matter where are they in labirinth and where are their entrances
- vector<vector<int>> RoomRandomPlacement(vector<int> roomSizeAndShape, vector<int> numOfEntrances, vector<int> entransesLength vector<vector<int>> square_);
+ vector<vector<int>> RoomRandomPlacement(vector<int> roomSizeAndShape, vector<int> numOfEntrances, vector<int> entransesLength, vector<vector<int>> square_);
  // true if in current position there is no wall that can be passed through without crossing other corridors
  bool Deadend(int x, int y, vector<vector<int>> square_);
+ // check negative coordinates
+ bool IsNegative(int x, int y, int index);
  // returns new position
- pair<int, int> Move(int x, int y, char whichWay, int howFar, vector<vector<int>> square_);
+ pair<int, int> Move(Random_Generator_ *Rand_gen, int x, int y, vector<vector<int>> square_);
  //if room can be made than make it and return random room space to continue digging
  //else return current position
  pair<int, int> MakeRoom(int x, int y, char whichWay, vector<vector<int>> square_);
+
+ bool CouldMakeCorridor(int x, int y, vector<vector<int>> square_);
+
+ int Direction(Random_Generator_ *Rand_gen);
+
+ int RightDirection(Random_Generator_ *Rand_gen, int x, int y, int direction, vector<vector<int>> square_);
+
+ bool IsRightDirection(int x, int y, int direction_, vector<vector<int>> square_);
+
+ int PaceLength(int difficulty_, Random_Generator_ *Rand_gen);
 
  vector<vector<int>> GetField();
 
